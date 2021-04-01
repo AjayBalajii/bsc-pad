300be30de7556e5dbe03e8db8d1a27f032ab66e9
Ajay <aajay693@gmail.com>
2021-04-01 13:17:25 +0530

    pro

--- src/secondpage.js
+++ src/secondpage.js
@@ -42,15 +42,38 @@ class secondpage extends Component{
     const balance = await web3.eth.getBalance(tokencontract.options.address);
     const totalsupply = await tokencontract.methods.totalSupply().call();
     const decimal = await TESTToken.methods.decimals().call();
-    const ooc = await TEST.methods.isTestOpen().call();
+    
+    
     const At = await TESTToken.methods.balanceOf("0x9C762c5F1A485d5c6EAaFbf128fe9ED71c908749").call();
     const name = await TESTToken.methods.name().call();
     const symbol = await TESTToken.methods.symbol().call();
     const balance_BUSD = await BEP20Token.methods.balanceOf(accounts[0]).call();
     const balance_TEST= await TESTToken.methods.balanceOf(accounts[0]).call();
 
+    const ooc = await TEST.methods.isTestOpen().call();
+    
+
+     
+
+     if (ooc==true){
+      document.getElementById("dem").innerHTML = "Open" ;
+
+      document.getElementById("ap").disabled=false;
+      document.getElementById("ap1").disabled=false;
+
+     }
+     else{
+      document.getElementById("dem").innerHTML = "Closed" ;
+
+     }
+     var p=At/100000000000000000;
+     p=p*100;
+     var p1=p.toFixed(2);
+     
+
+
   
-    this.setState({totalsupply,balance,name,symbol,decimal,balance_TEST,balance_BUSD,ooc,At});
+    this.setState({totalsupply,balance,name,symbol,decimal,balance_TEST,balance_BUSD,At,p1});
 
     
   }
@@ -71,7 +94,7 @@ class secondpage extends Component{
       });
     } 
 
-
+    
     const buyTest =async (event) => {
       
       event.preventDefault();
@@ -86,16 +109,7 @@ class secondpage extends Component{
      this.setState({amount});
     }
 
-    const click= async (event) =>{
-      event.preventDefault();
-
-      document.getElementById("ap").disabled=false;
-
-    }
-
-
-   
-
+    
     web3.givenProvider.enable().then(console.log);
     return (
       <div class="text" style={{backgroundImage:"url("+ Background +")",backgroundPosition: 'center',
@@ -114,10 +128,12 @@ class secondpage extends Component{
         <table>
           <div class="row justify-content-center">
             <div class="col-6">
-            <Card bg="dark" border="warning" style={{ width: '23rem' , padding: "30px" , borderRadius: "8%" }} >
+ <Card bg="dark" border="warning" style={{ width: '23rem' , padding: "25px" , borderRadius: "8%" }} >
         <p>
 
 Name <br/> {this.state.name}.
+</p>
+<p id="dem" class="pp">
 </p>
 <p>
 Symbol <br/> {this.state.symbol}.
@@ -126,18 +142,16 @@ Symbol <br/> {this.state.symbol}.
  Total Supply <br/> {this.state.totalsupply}. 
 </p>
 
-  <br/>  <br/>     
-       
+<p>
+          Decimals <br/> {this.state.decimal}.
+        </p>
 
         </Card>
 
             </div>
             <div class="col-2">
-            <Card bg="dark" border="warning" style={{ width: '25rem' , padding: "25px",borderRadius: "8%" }} bodyStyle={{}} >
+            <Card bg="dark" border="warning" style={{ width: '30rem' , padding: "25px",borderRadius: "9%" }} bodyStyle={{}} >
 
-            <p>
-          Decimals <br/> {this.state.decimal}.
-        </p>
         <p>
           Balance_BUSD <br/> {this.state.balance_BUSD}.
         </p>
@@ -147,8 +161,28 @@ Symbol <br/> {this.state.symbol}.
         <p>
           Available_Tokens <br/> {this.state.At}.
         </p>
-        
-        
+        <p class="p">Progress (Available Tokens)</p>
+        <progress id="file" value={this.state.At} max="100000000000000000" class="progress11"></progress>
+        <div>
+          <div class="container">
+            <div class="row">
+            <div class="col">
+            <p class="perci">
+          {this.state.p1}%
+        </p>
+            </div>
+              <div class="col align-self-end">
+              <p class="maxi">
+        {this.state.At}/100000000000000000.
+        </p>
+                      </div>
+            
+            </div>
+          </div>
+        </div>
+        <br/>
+       
+
         </Card>
 
             </div>
@@ -156,7 +190,7 @@ Symbol <br/> {this.state.symbol}.
         </table>
         <br/>   <br/> 
     
-          <div>
+          <div class="ma">
             <table>
               <div class="row">
                 <div class="col-6">
@@ -164,7 +198,7 @@ Symbol <br/> {this.state.symbol}.
 
                 </div>
                 <div class="col-2">
-                <button  onClick={buyTest} class="btn btn-outline-warning " id="buytestbutton" >buy test</button>
+                <button  onClick={buyTest} class="btn btn-outline-warning " id="ap1" disabled >buy test</button>
 
                 </div>
               </div>
@@ -173,14 +207,8 @@ Symbol <br/> {this.state.symbol}.
 
       
 <br/>
-<div>
-  <p>
-  OpenSlate <br/> {this.state.ooc}.
 
-  </p>
-  <button onClick={click} class="btn btn-outline-warning">check</button>
-</div>
-<br/><br/>   <br/>   </div>
+  </div>
     );
   }
 }
